1250f667cb3a8d6544577b441d31d2625e5eb72f
Jarand Westerheim <jarand.westerheim@gmail.com>
2019-10-21 13:31:12 +0200

    fix?

---
+++ games/agario/testingmap/map.js
@@ -0,0 +1,9 @@
+var canvas = document.querySelector("#canvas");
+var ctx = canvas.getContext("2d");
+var WIDTH = window.innerWidth || document.body.clientWidth;
+var HEIGHT = window.innerHeight || document.body.clientHeight;
+
+
+
+canvas.height = HEIGHT;
+canvas.width = WIDTH;
