98f799164b9142c00672123e570e7606256c7815
Jarand Westerheim <jarand.westerheim@gmail.com>
2020-05-05 12:07:06 +0200

    vectors made

--- games/space-test/p5/sketch.js
+++ games/space-test/p5/sketch.js
@@ -77,27 +77,15 @@ function drawStars() {
 //OBJECTS
 function generateObjects() {
   sun = {
-    pos:{
-      x:canvas.width/2,
-      y:canvas.height/2
-    },
-    vel:{
-      x:0,
-      y:0
-    },
+    pos:createVector(canvas.width/2,canvas.height/2),
+    vel:createVector(0,0),
     radius:60,
     color:"rgb(236, 240, 56)",
     mass:1.989e30
   };
   earth = {
-    pos:{
-      x:canvas.width/4,
-      y:canvas.height/2
-    },
-    vel:{
-      x:0,
-      y:0
-    },
+    pos:createVector(canvas.width/4,canvas.height/2),
+    vel:createVector(0,0),
     radius:20,
     color:"rgb(56, 141, 240)",
     mass:5.972e24
@@ -125,7 +113,6 @@ function updateEarth() {
 
 
   //acc
-  earth.vel.x += Number(gravForceEarth)/Number(earth.mass);
 
 
 
